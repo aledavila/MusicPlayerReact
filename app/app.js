78976177074b3271ec89504cfe2a9b5157e1c485
Alessandra Davila <alex.davila@utexas.edu>
2016-11-17 17:52:21 -0600

    adds player component

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Search from './components/search.component';
-import Details from './components/details.component'
+import Details from './components/details.component';
+import Player from './components/player.component';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div>
         <Search />
         <Details title={'Track title'} />
+        <Player />
       </div>
     );
   }
