1a441ec56400a40a90c939da363c721ca9ca9ba8
Alessandra Davila <alex.davila@utexas.edu>
2016-11-17 18:42:55 -0600

    creates footer

---
+++ app/components/footer.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+  render(){
+    return(
+      <div className="footer">
+        <p>Love from <img src="public/images/logo.png" className="logo" /> & <img src="public/images/soundcloud.png" className="soundcloud"/></p>
+      </div>
+    )
+  }
+
+}
+
+export default Footer
